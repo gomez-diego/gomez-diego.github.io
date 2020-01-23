bbfd47bec8045690f660850179afdc85c9ce05c0
Eric <gomez123098@yahoo.com>
2020-01-23 16:40:46 -0600

    Changes made to loading img
    Temp fix to loading images, added album cover option. Images now are displayed without 'changing' page.

--- file_manage.js
+++ file_manage.js
@@ -1,36 +1,36 @@
-const dir = ['./images/logos/', './images/illustrations/'];
+const dir = ['images/logos/', 'images/illustrations/', 'images/albumcovers/'];
 const fs = require('fs');
-let illustrations = {}
-let logos = {}
+var illustrations = {}
+var logos = {}
+var albumcovers = {}
 
 dir.forEach(directory => {
-    let index = 0;
-
+    var index = 0;
     try{
         fs.readdirSync(directory).forEach(file => {
-            let attributes = new Object();
+            var attributes = new Object();
             attributes["image"] = directory + file;
-            if (directory == './images/logos/')
+            if (directory == 'images/logos/')
                 logos[index] = attributes;
-            else
+            else if (directory == 'images/illustrations/')
                 illustrations[index] = attributes;
+            else
+                albumcovers[index] = attributes;
             index++;
         });
         console.log(directory," : complete.");
     } catch (err) {
         console.error(err);
     }
-    
-
-
 });
 
 illustrations = JSON.stringify(illustrations);
 logos = JSON.stringify(logos);
+albumcovers = JSON.stringify(albumcovers);
 
 try {
-    fs.writeFileSync('./scripts/files.js', 'let illustrations=' + illustrations + '\nlet logos=' + logos);
-    console.log('./scripts/files.js'," : File Created.");
+    fs.writeFileSync('scripts/files.js', 'var illustrations=' + illustrations + '\nvar logos=' + logos + '\nvar albumcovers=' + albumcovers);
+    console.log('scripts/files.js'," : File Created.");
 } catch (err) {
     console.error(err);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,58 @@
-let sidebar_active = true;
-let image_type;
+var sidebar_active = true;
+var image_type;
 
 $(document).ready(function () {
-    $("#home").on("click", () => home());
-    $("#illustrations").on("click", () => display_art(illustrations));
-    $("#logos").on("click", () => display_art(logos));
-    $("#contact").on("click", () => display_information(contact_info));
-    $("#about").on("click", () => display_information(about_me));
-    $(".side-bar_activate").on("click", () => side_bar());
+    $("#home").on("click", function () {
+        home();
+    });
+
+    $("#illustrations").on("click", function () {
+        display_art(illustrations);
+    });
+
+    $("#logos").on("click", function () { 
+        display_art(logos);
+    });
+
+    $("#albumcovers").on("click", function () { 
+        display_art(albumcovers);
+    });
+
+    $("#contact").on("click", function () {
+         display_information(contact_info);
+    });
+    
+    $("#about").on("click", function () { 
+        display_information(about_me);
+    });
+
+    $(".side-bar_activate").on("click", function () { 
+        side_bar();
+    });
 });
 
-let home = (x) => {
+var home = function (x) {
     $(".content-container").html("");
 }
 
-let display_information = (information) => {
+var display_information = function (information) {
     $(".content-container").removeClass('animated bounceInLeft');
     $(".main-container").removeClass("loading");
     $(".content-container").html(`<div class="words"></div>`);
 
-    setTimeout(x => {
-        information.forEach(element => {
-            let p = `<div class="fixed-words">${element}<div>`;
+    setTimeout(function(x) {
+        information.forEach(function(element) {
+            var p = `<div class="">${element}<div>`;
             $(".words").append(p);
         });
+
         $(".content-container").addClass('animated bounceInLeft');
+
     }, 250);
+
 }
 
-let side_bar = (x) => {
+var side_bar = (x) => {
     if (sidebar_active) {
         $(".side-bar").width("0px");
         $(".main-container").css("margin-left", "0px");
@@ -42,74 +66,59 @@ let side_bar = (x) => {
     image_adjust(image_type);
 }
 
-let display_image = (element) => {
-    //side_bar();
-    image_type = ".img_display";
-    let img = element.target.src;
-    $(".main-container").addClass("loading");
-    $(".content-container").html("");
-    $(".content-container").append(`<img src="${img}" class="img_display blur_loading"></img>`);
-    setTimeout(x => {
-        image_adjust(image_type); // HERE
-        $(".blur_loading").removeClass("blur_loading");
-        $(".main-container").removeClass("loading");
-        $(".main-container").addClass('animated fadeIn');
-    }, 250);
-    $(".main-container").removeClass('animated fadeIn');
-}
-
-let display_art = (currrent_object) => {
+var display_art = function (currrent_object) {
+    
     image_type = ".img_responsive";
-    $(".main-container").addClass("loading");
     $(".content-container").html("");
+
     for (const key in currrent_object) {
-        $(".content-container").append(`<img src="${currrent_object[key]["image"]}" class="img_responsive blur_loading"></img>`);
+        $(".content-container").append(`<img src="${currrent_object[key]["image"]}" class="img_responsive"></img>`);
     }
 
-    let imgs = document.images,
+    var imgs = document.images,
         len = imgs.length,
         counter = 0;
 
     [].forEach.call(imgs, function (img) {
-        img.addEventListener('load', incrementCounter, false);
-        img.addEventListener('error', incrementCounter, false);
+        img.addEventListener('load', loadImage, false);
+        img.addEventListener('error', loadImage, false);
         img.onerror = function () {
             $(img).remove();
         }
     });
 
-    function incrementCounter() {
-        counter++;
-        if (counter === len) {
-            image_adjust(image_type);
-            $(".main-container").removeClass("loading");
-            setTimeout(x => {
-                $(".main-container").addClass('animated fadeIn');
-                scroll_to_top();
-                $(".blur_loading").removeClass("blur_loading");
-            }, 500);
-            $(".main-container").removeClass('animated fadeIn');
-        }
+    function loadImage() {
+        image_adjust(image_type);
     }
-    $(".img_responsive").on("click", (x) => display_image(x));
+
+    $(".img_responsive").on("click", function(x){
+        $(".display-actived").remove();
+        let copy = $(this).clone().addClass("display-actived").removeClass("img_responsive").addClass("positioned");
+        var enclosure = $("<div>", {"class": "bg-container"});
+        enclosure.append(copy);
+        enclosure.insertAfter($(".main-container")).delay(500);
+        $(".bg-container").click(function() {
+            let copy = $(this);
+            copy.remove();
+        });
+    });
 
 }
 
-
-let image_adjust = (type_img) => {
-    let max_width = $(".content-container").width();
-    let max_height = $(window).innerHeight();
-    let elements = $(type_img);
+var image_adjust = (type_img) => {
+    var max_width = $(".content-container").width();
+    var max_height = $(window).innerHeight();
+    var elements = $(type_img);
 
     if (type_img == ".img_responsive") {
-        for (let index = 0; index < elements.length; index++) {
+        for (var index = 0; index < elements.length; index++) {
             $(elements[index]).css({
                 "width": max_width /3.75,
                 "height": "auto"
             });
         }
     } else {
-        for (let index = 0; index < elements.length; index++) {
+        for (var index = 0; index < elements.length; index++) {
             if(max_height >= max_width) {
                 $(elements[index]).css({
                     "width": max_width,
@@ -125,8 +134,9 @@ let image_adjust = (type_img) => {
     }
 }
 
-let opacity = (document.documentElement.scrollTop / 1500).toFixed(1);
-window.onscroll = () => {
+var opacity = (document.documentElement.scrollTop / 1500).toFixed(1);
+
+window.onscroll = function () {
     opacity = (document.documentElement.scrollTop / 1500).toFixed(1);
     if (opacity > 0.15) {
         document.getElementById("scroll").style.display = "block";
@@ -140,7 +150,7 @@ window.addEventListener("resize", function () {
     image_adjust(image_type);
 });
 
-let scroll_to_top = () => {
+var scroll_to_top = function () {
     $("html, body").animate({
         scrollTop: 0
     }, "slow");
